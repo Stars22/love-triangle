92566ccc4602495f9ba18f8f2f17d037d6f09379
unknown <maildeal@gmail.com>
2019-02-19 14:01:38 +0300

    implementation of loveTriangle func

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
     lover2 = preferences[lover1] - 1;
     lover3 = preferences[lover2] - 1;
     lover3Preference = preferences[lover3] - 1;
-    if(lover1 === lover2 || lover2 === lover3 || lover1 < 0 || lover2 < 0){
+    if(lover1 === lover2 || lover2 === lover3){
       continue;
     }
     if(lover1 === lover3Preference){
