9750708a7de152a01253ec2022b96a42224d715e
unknown <maildeal@gmail.com>
2019-02-19 13:06:57 +0300

    implementation of loveTriangle function

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@
 
 module.exports = function getLoveTrianglesCount(preferences = []) {
   let triangle = 0;
+  let lover1, lover2, lover3, lover3Preference;
   for(let i =0; i < preferences.length; i++){
-    let lover1 = i;
-    let lover2 = preferences[lover1] - 1;
-    let lover3 = preferences[lover2] - 1;
-    let lover3Preference = preferences[lover3] - 1;
+    lover1 = i;
+    lover2 = preferences[lover1] - 1;
+    lover3 = preferences[lover2] - 1;
+    lover3Preference = preferences[lover3] - 1;
     if(lover1 === lover2 || lover2 === lover3 || lover1 < 0 || lover2 < 0){
       continue;
     }
